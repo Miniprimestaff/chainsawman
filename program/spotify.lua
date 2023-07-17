3b61887d8e2c036578d1596793fa8173b003ecee
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-18 00:00:59 +0200

    Update spotify.lua
    Complete change of UI > Simple UI

--- program/spotify.lua
+++ program/spotify.lua
@@ -1,5 +1,3 @@
-local basalt = require("basalt")
-local aukit = require("aukit")
 local austream = shell.resolveProgram("austream")
 
 -- Téléchargement du fichier de la liste de lecture
@@ -18,43 +16,90 @@ if response then
       table.insert(musicList, entry.title)
     end
 
-    -- Création de la liste scrollable
-    local scrollableList = basalt.createScrollableFrame()
-    scrollableList:setTitle("Liste des musiques")
-
-    -- Ajout des musiques à la liste scrollable
-    for index, music in ipairs(musicList) do
-      local button = scrollableList:addButton():setText(music)
-
-      button:onClick(function(self, event, button, x, y)
-        if (event == "mouse_click") and (button == 1) then
-          local selectedMusic = playlist[index]
-          local selectedTitle = selectedMusic.title
-          local selectedURL = selectedMusic.link
-
-          -- Mise à jour du titre de la musique en cours
-          currentMusicTitle = selectedTitle
-          print("Lecture de la musique : " .. currentMusicTitle)
-
-          -- Lecture de la musique sélectionnée
-          shell.run(austream, selectedURL)
-
-          -- Attente jusqu'à la fin de la musique ou interruption de l'utilisateur
-          while true do
-            if not isPaused then
-              local status, result = pcall(aukit.isPlaying)
-              if not status or not result then
-                break
-              end
-            end
-            sleep(1)
-          end
-        end
-      end)
+    -- Fonction pour lire une musique
+    local function playMusic(title, musicURL)
+      print("Lecture de la musique : " .. title)
+      shell.run(austream, musicURL)
     end
 
-    term.clear()
-    scrollableList:draw()
+    -- Fonction pour afficher le menu de sélection de musique
+    local function displayMusicMenu()
+      local itemsPerPage = 6
+      local currentPage = 1
+      local totalOptions = #musicList
+      local totalPages = math.ceil(totalOptions / itemsPerPage)
+      local selectedIndex = 1
+
+      while true do
+        term.clear()
+        term.setCursorPos(1, 3)
+
+        -- Affichage du titre de l'application
+        term.setTextColor(colors.green)
+        term.setCursorPos(1, 2)
+        term.write(string.rep(string.char(143), term.getSize()))
+        term.setCursorPos(1, 3)
+        term.write(string.rep(" ", term.getSize()))
+        term.setCursorPos((term.getSize() - 7) / 2 + 1, 3)
+        term.write("Spotifo")
+        term.setCursorPos(1, 4)
+        term.write(string.rep(string.char(143), term.getSize()))
+
+        -- Calcul de l'index de début et de fin pour l'affichage de la page actuelle
+        local startIndex = (currentPage - 1) * itemsPerPage + 1
+        local endIndex = math.min(startIndex + itemsPerPage - 1, totalOptions)
+
+        for i = startIndex, endIndex do
+          local optionIndex = i - startIndex + 1
+          local option = musicList[i]
+
+          if optionIndex == selectedIndex then
+            term.setTextColor(colors.yellow) -- Couleur pour l'option sélectionnée
+            option = option .. " " -- Ajout d'un espace à la fin de l'option
+          else
+            term.setTextColor(colors.white) -- Couleur par défaut pour les autres options
+          end
+
+          print(optionIndex, " [" .. option .. "]")
+        end
+
+        term.setTextColor(colors.white) -- Couleur blanche pour le texte de sélection
+        print()
+
+        -- Affichage des flèches
+        term.setCursorPos(1, itemsPerPage + 7)
+        term.write(string.char(17)) -- Flèche gauche (16)
+        term.setCursorPos(term.getSize(), itemsPerPage + 7)
+        term.write(string.char(16)) -- Flèche droite (17)
+
+        local _, key = os.pullEvent("key")
+
+        if key == keys.up then
+          selectedIndex = selectedIndex - 1
+          if selectedIndex < 1 then
+            selectedIndex = endIndex - startIndex + 1
+          end
+        elseif key == keys.down then
+          selectedIndex = selectedIndex + 1
+          if selectedIndex > endIndex - startIndex + 1 then
+            selectedIndex = 1
+          end
+        elseif key == keys.left and currentPage > 1 then
+          currentPage = currentPage - 1
+          selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
+        elseif key == keys.right and currentPage < totalPages then
+          currentPage = currentPage + 1
+          selectedIndex = math.min(selectedIndex, endIndex - startIndex + 1)
+        elseif key == keys.enter then
+          local selectedOption = startIndex + selectedIndex - 1
+          local selectedMusic = playlist[selectedOption]
+          playMusic(selectedMusic.title, selectedMusic.link)
+        end
+      end
+    end
+
+    -- Affichage initial du menu de sélection de musique
+    displayMusicMenu()
   else
     print("Erreur de parsing du fichier de la liste de lecture.")
   end
