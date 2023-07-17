a1c817ceff362ed86e62dc9c89327b952af01f16
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-17 06:02:44 +0200

    Update spotify.lua
    scroll

--- program/spotify.lua
+++ program/spotify.lua
@@ -1,5 +1,6 @@
 local aukit = require("aukit")
 local austream = shell.resolveProgram("austream")
+local term = require("term")
 
 -- Téléchargement du fichier de la liste de lecture
 local playlistURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlist.json"
@@ -13,69 +14,65 @@ if response then
   if success and type(playlist) == "table" then
     -- Création de la liste sélectionnable des musiques
     local musicList = {}
-    for i, entry in ipairs(playlist) do
-      local title = entry.title
-      table.insert(musicList, {index = i, title = title})
+    for _, entry in ipairs(playlist) do
+      table.insert(musicList, entry.title)
+    end
+
+    -- Fonction pour lire une musique
+    local function playMusic(title, musicURL)
+      -- Lecture de la musique en utilisant AUStream
+      shell.run(austream, musicURL)
+      
+      -- Affichage du titre de la musique en cours de lecture
+      print("Lecture de la musique : " .. title)
+      
+      -- Attente jusqu'à la fin de la musique
+      while true do
+        local status, result = pcall(aukit.isPlaying)
+        if not status or not result then
+          break
+        end
+        sleep(1)
+      end
     end
 
     -- Variables de contrôle
     local isPaused = false
     local currentMusicTitle = ""
 
-    -- Affichage de la liste des musiques sélectionnables
-    print("Liste des musiques :")
-    for _, music in ipairs(musicList) do
-      print(music.index .. ". " .. music.title)
-    end
+    -- Variables pour le défilement de la liste
+    local maxLines = 10 -- Nombre maximal de lignes à afficher
+    local startIndex = 1 -- Indice de départ pour afficher les musiques
+    local endIndex = math.min(#musicList, startIndex + maxLines - 1) -- Indice de fin pour afficher les musiques
 
-    -- Demande à l'utilisateur de sélectionner une musique
-    local selectedMusicIndex = tonumber(read())
-    if selectedMusicIndex and selectedMusicIndex >= 1 and selectedMusicIndex <= #musicList then
-      local selectedMusic = playlist[musicList[selectedMusicIndex].index]
-      local selectedTitle = selectedMusic.title
-      local selectedURL = selectedMusic.link
-
-      -- Mise à jour du titre de la musique en cours
-      currentMusicTitle = selectedTitle
-      print("Lecture de la musique : " .. currentMusicTitle)
-
-      -- Lecture de la musique sélectionnée
-      shell.run(austream, selectedURL)
-      
-      -- Attente jusqu'à la fin de la musique ou interruption de l'utilisateur
-      while true do
-        if not isPaused then
-          local status, result = pcall(aukit.isPlaying)
-          if not status or not result then
-            break
-          end
-        end
-        sleep(1)
+    -- Gestion de l'événement de défilement de la souris
+    local function handleScrollEvent(direction)
+      if direction == -1 and startIndex > 1 then
+        startIndex = startIndex - 1
+        endIndex = endIndex - 1
+        term.scroll(1) -- Défilement d'une ligne vers le haut
+      elseif direction == 1 and endIndex < #musicList then
+        startIndex = startIndex + 1
+        endIndex = endIndex + 1
+        term.scroll(-1) -- Défilement d'une ligne vers le bas
       end
-    else
-      print("Index de musique invalide.")
     end
 
-    -- Boucle principale pour gérer les commandes utilisateur
-    while true do
-      term.setCursorPos(1, 2) -- Positionne le curseur au-dessus de la barre de progression
-      term.clearLine() -- Efface la ligne
-      term.write("Musique en cours : " .. currentMusicTitle) -- Affiche le nom de la musique en cours
+    -- Affichage initial de la liste des musiques
+    term.clear()
+    for i = startIndex, endIndex do
+      print(i .. ". " .. musicList[i])
+    end
 
-      local command = read()
-      if command == "pause" then
-        -- Mettre en pause la musique
-        isPaused = true
-        print("Musique en pause : " .. currentMusicTitle)
-      elseif command == "resume" then
-        -- Reprendre la lecture de la musique
-        isPaused = false
-        print("Reprise de la musique : " .. currentMusicTitle)
-      elseif command == "stop" then
-        -- Interruption de l'utilisateur
-        break
-      else
-        print("Commande non valide.")
+    while true do
+      -- Attente d'un événement de défilement de la souris
+      local _, scrollDirection = os.pullEvent("mouse_scroll")
+      handleScrollEvent(scrollDirection)
+
+      -- Affichage mis à jour de la liste des musiques
+      term.clear()
+      for i = startIndex, endIndex do
+        print(i .. ". " .. musicList[i])
       end
     end
   else
