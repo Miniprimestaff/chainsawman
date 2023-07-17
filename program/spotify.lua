005f9ade4a0eedcdd22d95588d64cd58173a9316
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-17 06:50:04 +0200

    Update spotify.lua

--- program/spotify.lua
+++ program/spotify.lua
@@ -1,6 +1,6 @@
 local aukit = require("aukit")
 local austream = shell.resolveProgram("austream")
-local term = require("term")
+local button = require("button")
 
 -- Téléchargement du fichier de la liste de lecture
 local playlistURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlist.json"
@@ -18,6 +18,22 @@ if response then
       table.insert(musicList, entry)
     end
 
+    -- Création des boutons pour chaque musique
+    local buttons = {}
+    for i, music in ipairs(musicList) do
+      local button = button.create(music.title)
+      button:setPos(1, i)
+      button:setSize(10, 1)
+      button:setColor(colors.red)
+
+      -- Callback lorsque le bouton est cliqué
+      button:onClick(function()
+        playMusic(music.title, music.link)
+      end)
+
+      table.insert(buttons, button)
+    end
+
     -- Fonction pour lire une musique
     local function playMusic(title, musicURL)
       -- Lecture de la musique en utilisant AUStream
@@ -27,67 +43,18 @@ if response then
       print("Lecture de la musique : " .. title)
       
       -- Attente jusqu'à la fin de la musique
-      while aukit.isPlaying() do
+      while true do
+        local status, result = pcall(aukit.isPlaying)
+        if not status or not result then
+          break
+        end
         sleep(1)
       end
     end
 
-    -- Variables de contrôle
-    local selectedPosition = 1
-
-    -- Variables pour le défilement de la liste
-    local maxLines = 10 -- Nombre maximal de lignes à afficher
-    local startIndex = 1 -- Indice de départ pour afficher les musiques
-    local endIndex = math.min(#musicList, startIndex + maxLines - 1) -- Indice de fin pour afficher les musiques
-
-    -- Gestion de l'événement de défilement de la souris
-    local function handleScrollEvent(direction)
-      if direction == -1 and selectedPosition > 1 then
-        selectedPosition = selectedPosition - 1
-        if startIndex > 1 then
-          startIndex = startIndex - 1
-          endIndex = endIndex - 1
-          term.scroll(1) -- Défilement d'une ligne vers le haut
-        end
-      elseif direction == 1 and selectedPosition < maxLines and startIndex + selectedPosition <= #musicList then
-        selectedPosition = selectedPosition + 1
-        if endIndex < #musicList then
-          startIndex = startIndex + 1
-          endIndex = endIndex + 1
-          term.scroll(-1) -- Défilement d'une ligne vers le bas
-        end
-      end
-    end
-
     -- Affichage initial de la liste des musiques
-    term.clear()
-    for i = startIndex, endIndex do
-      local music = musicList[i]
-      print(i .. ". " .. music.title)
-    end
+    button.await(buttons)
 
-    while true do
-      -- Attente d'un événement de défilement de la souris
-      local _, scrollDirection = os.pullEvent("mouse_scroll")
-      handleScrollEvent(scrollDirection)
-
-      -- Sélection de la musique et lecture
-      local selectedMusicIndex = startIndex + selectedPosition - 1
-      if selectedMusicIndex >= 1 and selectedMusicIndex <= #musicList then
-        local selectedMusic = musicList[selectedMusicIndex]
-        local selectedTitle = selectedMusic.title
-        local selectedURL = selectedMusic.link
-        -- Lecture de la musique sélectionnée
-        playMusic(selectedTitle, selectedURL)
-      end
-
-      -- Affichage mis à jour de la liste des musiques
-      term.clear()
-      for i = startIndex, endIndex do
-        local music = musicList[i]
-        print(i .. ". " .. music.title)
-      end
-    end
   else
     print("Erreur de parsing du fichier de la liste de lecture.")
   end
