13ffbaf2d91301312a5bd8fd1c24573cf65268f2
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-17 07:04:56 +0200

    Update spotify.lua

--- program/spotify.lua
+++ program/spotify.lua
@@ -1,6 +1,5 @@
 local aukit = require("aukit")
 local austream = shell.resolveProgram("austream")
-local buttonApi = require("button")
 
 -- Téléchargement du fichier de la liste de lecture
 local playlistURL = "https://raw.githubusercontent.com/Miniprimestaff/music-cc/main/program/playlist.json"
@@ -15,54 +14,92 @@ if response then
     -- Création de la liste sélectionnable des musiques
     local musicList = {}
     for _, entry in ipairs(playlist) do
-      table.insert(musicList, entry)
+      table.insert(musicList, entry.title)
     end
 
-    -- Création des boutons pour chaque musique
-    local buttons = {}
-    for i, music in ipairs(musicList) do
-      local btn = buttonApi.create(music.title)
-      btn:setPos(1, i)
-      btn:setSize(10, 1)
-      btn:setColor(colors.red)
+    -- Variables de contrôle
+    local isPaused = false
+    local currentMusicTitle = ""
 
-      -- Callback lorsque le bouton est cliqué
-      btn:onClick(function()
-        playMusic(music.title, music.link)
-      end)
+    -- Variables pour le défilement de la liste
+    local maxLines = 10 -- Nombre maximal de lignes à afficher
+    local startIndex = 1 -- Indice de départ pour afficher les musiques
+    local endIndex = math.min(#musicList, startIndex + maxLines - 1) -- Indice de fin pour afficher les musiques
 
-      table.insert(buttons, btn)
-    end
-
-    -- Fonction pour lire une musique
-    local function playMusic(title, musicURL)
-      -- Lecture de la musique en utilisant AUStream
-      shell.run(austream, musicURL)
-      
-      -- Affichage du titre de la musique en cours de lecture
-      print("Lecture de la musique : " .. title)
-      
-      -- Attente jusqu'à la fin de la musique
-      while true do
-        local status, result = pcall(aukit.isPlaying)
-        if not status or not result then
-          break
-        end
-        sleep(1)
+    -- Fonction pour afficher la liste des musiques
+    local function printMusicList()
+      term.clear()
+      print("Liste des musiques :")
+      for i = startIndex, endIndex do
+        print(i .. ". " .. musicList[i])
       end
     end
 
-    -- Attente de l'événement de clic sur les boutons
+    -- Affichage initial de la liste des musiques
+    printMusicList()
+
+    -- Boucle pour gérer le défilement de la liste
     while true do
-      local clickedButton = buttonApi.await(buttons)
-      
-      if clickedButton then
-        -- Récupération de la musique associée au bouton cliqué
-        local music = musicList[clickedButton:getPos().y]
-        playMusic(music.title, music.link)
-      end
-    end
+      local command = read()
 
+      if command == "up" then
+        -- Défilement vers le haut
+        if startIndex > 1 then
+          startIndex = startIndex - 1
+          endIndex = endIndex - 1
+        end
+      elseif command == "down" then
+        -- Défilement vers le bas
+        if endIndex < #musicList then
+          startIndex = startIndex + 1
+          endIndex = endIndex + 1
+        end
+      elseif tonumber(command) then
+        -- Sélection d'une musique à jouer
+        local selectedIndex = tonumber(command)
+        if selectedIndex >= startIndex and selectedIndex <= endIndex then
+          local selectedMusic = playlist[selectedIndex]
+          local selectedTitle = selectedMusic.title
+          local selectedURL = selectedMusic.link
+
+          -- Mise à jour du titre de la musique en cours
+          currentMusicTitle = selectedTitle
+          print("Lecture de la musique : " .. currentMusicTitle)
+
+          -- Lecture de la musique sélectionnée
+          shell.run(austream, selectedURL)
+
+          -- Attente jusqu'à la fin de la musique ou interruption de l'utilisateur
+          while true do
+            if not isPaused then
+              local status, result = pcall(aukit.isPlaying)
+              if not status or not result then
+                break
+              end
+            end
+            sleep(1)
+          end
+        else
+          print("Index de musique invalide.")
+        end
+      elseif command == "pause" then
+        -- Mettre en pause la musique en cours
+        isPaused = true
+        print("Musique en pause : " .. currentMusicTitle)
+      elseif command == "resume" then
+        -- Reprendre la lecture de la musique en cours
+        isPaused = false
+        print("Reprise de la musique : " .. currentMusicTitle)
+      elseif command == "stop" then
+        -- Interruption de l'utilisateur
+        break
+      else
+        print("Commande non valide.")
+      end
+
+      -- Affichage mis à jour de la liste des musiques
+      printMusicList()
+    end
   else
     print("Erreur de parsing du fichier de la liste de lecture.")
   end
